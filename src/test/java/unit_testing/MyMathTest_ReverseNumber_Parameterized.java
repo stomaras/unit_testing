9cb1345d574d23c7d7d923d4c607329e1cd7cc21
stomaras <sptomaras@yahoo.gr>
2018-04-17 16:26:44 +0300

    Create MyMathTest_ReverseNumber_Parameterized.java
    this class test the reverse number method from MyMath class

---
+++ src/test/java/unit_testing/MyMathTest_ReverseNumber_Parameterized.java
@@ -0,0 +1,48 @@
+package unit_testing;
+
+import org.junit.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+// the runwith annotation help us to run the parameterized class with the MyMath_ReverseNumber_Parameterized
+@RunWith(Parameterized.class)
+public class MyMathTest_ReverseNumber_Parameterized {
+	private int expectedResult;
+	private int val;
+	MyMath cal;
+	
+	// i add the before annotation which run before every method 
+	@Before
+	public void setup(){
+		cal = new MyMath();
+	}
+	
+	// the constructor 
+	public MyMathTest_ReverseNumber_Parameterized(int expectedResult, int val){
+		this.val = val;
+		this.expectedResult = expectedResult;
+		
+	}
+	
+	// i add the parameters annotation 
+	@Parameters
+	public static Collection<Object[]> testData(){
+		Object[][] data = new Object[][]{{1,-1},{0,0},{-2,2}};
+		return Arrays.asList(data);
+	}
+	@Test
+	public void testAdd(){
+		// i put in assertEquals the expectedresult and actual result 
+		Assert.assertEquals(expectedResult, cal.reverseNumber(val));
+	}
+}
+
+
+	
+
+
