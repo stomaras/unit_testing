1013d6b19433298451d3fa0987c84ecfdc589557
stomaras <sptomaras@yahoo.gr>
2018-04-17 16:28:26 +0300

    Create SuiteTest.java
    this is a suite class who run at the same time 2 other classes

---
+++ src/test/java/unit_testing/SuiteTest.java
@@ -0,0 +1,13 @@
+package unit_testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+// the RunWith annotation help us to run Suite class together with SuiteTest
+@RunWith(Suite.class)
+// the SuiteClasses annotation nhelp us to run at the same time many classes 
+@SuiteClasses({MyMathTest.class,MyMathTest_ReverseNumber_Parameterized.class})
+public class SuitTest {
+
+}
