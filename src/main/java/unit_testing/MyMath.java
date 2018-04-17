70806d359dbb38a28b67af0a96f8022771f5044e
stomaras <sptomaras@yahoo.gr>
2018-04-17 16:06:04 +0300

    Create MyMath.java
    This MyMath class which do the division and return the reverse number

---
+++ src/main/java/unit_testing/MyMath.java
@@ -0,0 +1,34 @@
+package unit_testingg;
+
+public class MyMath {
+	
+		// i make a default constructor 
+		MyMath(){
+			
+		}
+		
+		//i implement the method which return the reverse number 
+		public int reverseNumber(int number)
+		{
+			if(number < 0){
+				number = Math.abs(number);
+			}
+			else if(number > 0){
+				number = -(number);
+			}else{
+				return number;
+			}
+			return number;	
+		}
+		
+		// i implement  the method which return the division of num and denom 
+		public  double divide(int num, int denom){
+				if(denom ==0){
+					throw new IllegalArgumentException("cannot divide by 0");
+				}
+				return (double)num/(double)denom;
+		}
+	 
+	 
+		
+	}
