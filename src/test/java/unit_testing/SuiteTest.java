c00103b4b96a0685a08599b32e75a6ea90e61c36
stomaras <sptomaras@yahoo.gr>
2018-04-19 13:31:33 +0300

    Update SuiteTest.java

--- src/test/java/unit_testing/SuiteTest.java
+++ src/test/java/unit_testing/SuiteTest.java
@@ -3,8 +3,13 @@ package unit_testing;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-// the RunWith annotation help us to run Suite class together with SuiteTest
+/**
+* @author tomaras spyros
+* @since 19/04/2018
+* the RunWith annotation help us to run a Suite 
+* which help us to run at the same time 2 classes 
+*the MyMathTest.class and the MyMathTest_ReverseNumber_Parameterized.class
+*/ 
 @RunWith(Suite.class)
 // the SuiteClasses annotation nhelp us to run at the same time many classes 
 @Suite.@SuiteClasses({MyMathTest.class,MyMathTest_ReverseNumber_Parameterized.class})
