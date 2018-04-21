521c1a1abace1e10277e7f675e84178bfafd173a
stomaras <sptomaras@yahoo.gr>
2018-04-21 12:18:20 +0300

    Update SuiteTest.java

--- src/test/java/unit_testing/SuiteTest.java
+++ src/test/java/unit_testing/SuiteTest.java
@@ -2,7 +2,7 @@ package unit_testing;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+
 /**
 * @author tomaras spyros
 * @since 19/04/2018
